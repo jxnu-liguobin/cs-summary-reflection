29ccc9b60c7525939942ad67405a846e0fb3b742
jxnu-liguobin <568845948@qq.com>
2018-08-27 23:18:09 +0800

    摸鱼

---
+++ src/cn/edu/jxnu/leetcode/scala/dp/CountBits.scala
@@ -0,0 +1,63 @@
+package cn.edu.jxnu.leetcode.scala.dp
+
+
+/**
+  * 给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。
+  *
+  * @title: CountBits
+  * @description:
+  * @author 梦境迷离
+  * @date 2018-08-27
+  */
+object CountBits extends App {
+
+  val ret = countBits(85723)
+  for (i <- ret) {
+    print(i)
+  }
+
+
+  /**
+    *
+    * f[1] = (f[0]==0) + (1%1==1) = 1
+    * f[11] = (f[1]==1) + (11%1==1)  = 2
+    * f[110] = (f[11]==2) + (110%1==0) = 2
+    * f[1101] = (f[110] ==2) + (1101%1==1) =3;
+    */
+  def countBits(num: Int): Array[Int] = {
+    val f = new Array[Int](num + 1);
+    for (i <- 1 to num) {
+      f(i) = f(i >> 1) + (i & 1)
+    }
+    f
+  }
+
+
+  /**
+    * 最后用例超时
+    */
+  def countBits2(num: Int): Array[Int] = {
+    var ret = new Array[Int](num + 1)
+    for (i <- 0 to num) {
+      ret(i) = count(i)
+    }
+    ret
+  }
+
+
+  def count(n: Int): Int = {
+    import scala.util.control.Breaks.{break, breakable}
+    var nums = n
+    var count = 0
+    while (nums != 0) {
+      breakable {
+        if ((nums & 1) == 1) {
+          count += 1
+          break()
+        }
+      }
+      nums >>>= 1
+    }
+    count
+  }
+}
--- src/cn/edu/jxnu/practice/LIS.java
+++ src/cn/edu/jxnu/practice/LIS.java
@@ -14,46 +14,47 @@ package cn.edu.jxnu.practice;
  * <p>
  * School: jxnu
  * </p>
- * @require 时间复杂度尽可能低
+ *
  * @author Mr.Li
- * @date 2018-2-16
  * @version 1.0
+ * @require 时间复杂度尽可能低
+ * @date 2018-2-16
  */
 public class LIS {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		int[] array = { 1, -1, 2, -3, 4, -5, 6, -7 };
-		int result = new LIS().lis(array);
-		System.out.println("最大递增子序列长度是：" + result);
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int result = new LIS().lis(array);
+        System.out.println("最大递增子序列长度是：" + result);
 
-	}
+    }
 
-	/**
-	 * @description 动态规划 基础
-	 * @param array
-	 * @return
-	 */
-	public int lis(int[] array) {
-		int[] lis = new int[array.length];
-		for (int i = 0; i < array.length; i++) {
-			lis[i] = 1;
-			for (int j = 0; j < i; j++) {
-				if (array[i] > array[j] && lis[j] + 1 > lis[i]) {
-					lis[i] = lis[j] + 1;
-				}
-			}
-		}
-		// 取得lis最大值
-		int max = -1;
-		for (int i = 0; i < lis.length; i++) {
-			if (lis[i] > max) {
-				max = lis[i];
-			}
-		}
+    /**
+     * @param array
+     * @return
+     * @description 动态规划 基础
+     */
+    public int lis(int[] array) {
+        int[] lis = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            lis[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (array[i] > array[j] && lis[j] + 1 > lis[i]) {
+                    lis[i] = lis[j] + 1;
+                }
+            }
+        }
+        // 取得lis最大值
+        int max = -1;
+        for (int i = 0; i < lis.length; i++) {
+            if (lis[i] > max) {
+                max = lis[i];
+            }
+        }
 
-		return max;
-	}
+        return max;
+    }
 }
