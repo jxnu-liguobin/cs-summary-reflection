62bfa504cc04e30a5752b545970c6c78e73f6f5a
jxnu-liguobin <568845948@qq.com>
2018-12-05 21:55:01 +0800

    scala基础语法 - 集合

--- src/cn/edu/jxnu/scala/basic/package.scala
+++ src/cn/edu/jxnu/scala/basic/package.scala
@@ -1,6 +1,6 @@
 package cn.edu.jxnu.scala
 
-import scala.collection.mutable._
+import scala.collection.mutable.ListBuffer
 
 /**
   * 本包存放scala学习笔记
@@ -277,5 +277,25 @@ object Test7 extends App {
 }
 
 object Test8 extends App {
+
+
+    println("================Set集===========")
+    //创建并初始化，不可变Set集合，类似数组，也是调用了伴生对象的apply方法
+    var set = Set("hello", "world") //默认集合都是不可变的， 其他集：var hashSet = HashSet("hello","world")
+    set += "hhh" //实际是“+”方法[set = set + "hhh"]， 创建并返回新集合，无论是可变还是不可变集
+    println(set.contains("hello"))
+    // 注意：因为对不可变集使用+=实际是调用+=方法set1.+=("hhh")，所以表达式不需要重新赋值，set1可以是val
+    // 而对于可变集，+=实际是+调用后再进行赋值，所以不能为val
+    //对于只有一个参数的方法调用可以省略 . () ，所以set1.+=("hhh") ===> set1 += "hhh"
+    val set1 = scala.collection.mutable.Set("hello", "world") //提供+=方法
+    set1 += "hhh" //实际是调用方法
+    println(set1.contains("hhh"))
+    println("================Map集===========")
+    //创建并初始化可变的Map
+    val map = scala.collection.mutable.Map[String, String]()
+    map += ("a" -> "b") //实际是(a).->("b")方法调用，同样也是省略了. ()
+    map += ("b" -> "c") //底层也是+=的方法调用，所以map也可以是val
+    map += ("c" -> "d") //Map("hello"->"world","a"->"b")默认是不可变，不再解释
+    println(map("b"))
 
 }
