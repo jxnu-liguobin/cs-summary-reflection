ae86b4423f3ea5a6c5479c9d30451739c7d3764a
梦境迷离 <568845948@qq.com>
2019-08-24 21:58:59 +0800

    增加Scala 函数

---
+++ src/main/java/cn/edu/jxnu/utils/play/PerformanceRecordUtil.scala
@@ -0,0 +1,40 @@
+package cn.edu.jxnu.utils.play
+
+import com.typesafe.scalalogging.LazyLogging
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+/**
+ * 异步请求记录
+ *
+ * @author 梦境迷离
+ * @since 2019-08-24
+ * @version v1.0
+ */
+object PerformanceRecordUtil extends LazyLogging {
+  private val durationLimit = 100.millisecond.toMillis
+
+  implicit class FutureWrapper[T](f: Future[T]) {
+
+    /**
+     * val a = Future {
+     *   Thread.sleep(1000)
+     * }
+     * a.elapsed("a future")
+     *
+     * @param invokeTag
+     * @return
+     */
+    def elapsed(invokeTag: String): Future[T] = {
+      val begin = System.currentTimeMillis()
+      f.map { result ⇒
+        val duration = System.currentTimeMillis() - begin
+        if (duration > durationLimit) logger.warn(s"slow invoked: [${invokeTag}] elapsed [${System.currentTimeMillis() - begin}].ms")
+        result
+      }
+    }
+  }
+
+}
