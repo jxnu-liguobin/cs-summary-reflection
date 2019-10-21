132e9f3148e8c276ebba6c7aa63d2fcc610c731a
梦境迷离 <568845948@qq.com>
2019-10-21 09:59:13 +0800

    Update baidu_push.js

--- docs/public/js/baidu_push.js
+++ docs/public/js/baidu_push.js
@@ -1,3 +1,4 @@
+//百度爬虫被屏蔽了，码云免费版不支持自定义域名，这个推送实际无效
 (function () {
     var bp = document.createElement('script');
     var curProtocol = window.location.protocol.split(':')[0];
@@ -10,19 +11,19 @@
     s.parentNode.insertBefore(bp, s);
 })();
 
+//google
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-147390701-1');
+
+//baidu
 var _hmt = _hmt || [];
 (function () {
     var hm = document.createElement("script");
     hm.src = "https://hm.baidu.com/hm.js?dd381827cc51b62781961a344a8c16ea";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
-    var g = document.createElement("script");
-    g.src = "https://ww.googletagmanager.com/gtag/js?id=UA-147390701-1";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(g, s);
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-147390701-1');
 })();
 //百度爬虫被屏蔽了，码云免费版不支持自定义域名
+
