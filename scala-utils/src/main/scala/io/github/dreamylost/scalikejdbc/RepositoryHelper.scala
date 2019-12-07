41e646a654baa714ef954f4c4e2c5181b70184c0
梦境迷离 <dreamylost@outlook.com>
2019-12-07 23:00:28 +0800

    新增scalikejdbc utils，时间：2019-12-07 23:00:28

---
+++ scala-utils/src/main/scala/io/github/dreamylost/scalikejdbc/RepositoryHelper.scala
@@ -0,0 +1,44 @@
+package io.github.dreamylost.scalikejdbc
+
+import java.sql.ResultSet
+
+import io.github.dreamylost.other.JacksonScalaSupport
+import org.postgresql.util.PGobject
+import scalikejdbc.{ ParameterBinderFactory, TypeBinder }
+
+/**
+ * 使得scalikejdbc的类型支持pgsql的json
+ *
+ * @author 梦境迷离
+ * @time 2019-12-07
+ * @version v1.0
+ */
+object RepositoryHelper {
+
+  //使得scalikejdbc的类型支持pgsql的json
+  object ParameterBinders {
+
+    implicit val targetFields: ParameterBinderFactory[Map[String, String]] = ParameterBinderFactory[Map[String, String]] {
+      value =>
+        (stmt, idx) => {
+          val obj = new PGobject()
+          obj.setType("json")
+          obj.setValue(JacksonScalaSupport.mapper.writeValueAsString(value))
+          stmt.setObject(idx, obj)
+        }
+    }
+  }
+
+  //使得scalikejdbc支持查询映射到entity的Map参数有
+  object TypeBinders {
+
+    implicit val targetFieldsTypeBinder: TypeBinder[Map[String, String]] = new TypeBinder[Map[String, String]] {
+      def apply(rs: ResultSet, label: String): Map[String, String] =
+        JacksonScalaSupport.mapper.readValue(rs.getString(label), classOf[Map[String, String]])
+
+      def apply(rs: ResultSet, columnIndex: Int): Map[String, String] =
+        JacksonScalaSupport.mapper.readValue(rs.getString(columnIndex), classOf[Map[String, String]])
+    }
+  }
+
+}
