f89afb7d7808b41cd1d92ad5f4ef81bb896f824a
梦境迷离 <dreamylost@outlook.com>
2019-12-07 23:13:18 +0800

    fix ，时间：2019-12-07 23:13:18

--- scala-utils/src/main/scala/io/github/dreamylost/play/ImplicitUtils.scala
+++ scala-utils/src/main/scala/io/github/dreamylost/play/ImplicitUtils.scala
@@ -3,8 +3,7 @@ package io.github.dreamylost.play
 import play.api.data.FormError
 import play.api.data.Forms._
 import play.api.data.format.Formatter
-import play.api.http.{ ContentTypeOf, ContentTypes, Writeable }
-import play.api.libs.json.{ JsNull, JsValue, Json, Writes }
+import play.api.libs.json.{ JsNull, JsValue, Json }
 
 /**
  * 编解码工具
@@ -13,23 +12,6 @@ import play.api.libs.json.{ JsNull, JsValue, Json, Writes }
  * @version 1.0, 2019-07-15
  */
 object ImplicitUtils {
-
-  /** 将DTO写入流，所需要的隐式参数
-   *
-   * {{{
-   *   Ok(dto)
-   * }}}
-   *
-   * @param writes
-   * @param codec
-   * @tparam A
-   * @return
-   */
-  implicit def jsonWritable[A](implicit writes: Writes[A], codec: play.api.mvc.Codec): Writeable[A] = {
-    implicit val contentType = ContentTypeOf[A](Some(ContentTypes.JSON))
-    val transform = Writeable.writeableOf_JsValue.transform compose writes.writes
-    Writeable(transform)
-  }
 
   /**
    * Play 表单类型映射，使用
