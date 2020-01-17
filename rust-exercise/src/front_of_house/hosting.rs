15a4e74b8832fa949ec1daebe4bb62e72058a44c
梦境迷离 <liguobin@growingio.com>
2020-01-17 19:10:46 +0800

    add rust module

--- rust-exercise/src/front_of_house/hosting.rs
+++ rust-exercise/src/front_of_house/hosting.rs
@@ -1,4 +1,4 @@
-//hosting是front_of_house的子模块，hosting在front_of_house的文件夹里面
+//hosting是front_of_house的子模块，hosting在front_of_house的文件夹里面，使用fn公开方法
 pub fn add_to_waitlist_host() {
     println!("hello world aaaaaaaaaaaa")
 }
