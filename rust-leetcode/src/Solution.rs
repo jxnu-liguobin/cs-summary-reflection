7acb52336f2cb40fc6dd54ce9aa43488e9414c31
梦境迷离 <568845948@qq.com>
2020-03-10 19:56:28 +0800

    add rust-leetcode

--- rust-leetcode/src/Solution.rs
+++ rust-leetcode/src/Solution.rs
@@ -4,10 +4,12 @@ use std::ops::Index;
 ///Leetcode 超简单的算法题目，主要为了熟悉rust语法
 pub fn solutions() {
     interview_58_2();
+    leetcode_1365();
 }
 
+struct Solution;
+
 fn interview_58_2() {
-    struct Solution;
     impl Solution {
         pub fn reverse_left_words(s: String, n: i32) -> String {
             let mut s1 = String::from(&s[0..n as usize]);
@@ -19,4 +21,28 @@ fn interview_58_2() {
 
     let result = Solution::reverse_left_words("abcdefg".to_owned(), 2);
     println!("{}", result);
+}
+
+fn leetcode_1365() {
+    impl Solution {
+        pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
+            let mut ret = Vec::with_capacity(nums.len());
+            for i in 0..nums.len() {
+                let mut count = 0;
+                for j in 0..nums.len() {
+                    if nums[i] > nums[j] {
+                        count += 1;
+                    }
+                }
+                ret.push(count);
+            }
+            ret
+        }
+    }
+
+    let nums = vec![8, 1, 2, 2, 3];
+    let result = Solution::smaller_numbers_than_current(nums);
+    for e in result.iter() {
+        print!("{}", e);
+    }
 }
