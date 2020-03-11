6b18ab9fb7614793816a4cb40c86ec93e21fb1a6
梦境迷离 <568845948@qq.com>
2020-03-11 19:29:24 +0800

    add rust-leetcode

--- rust-leetcode/src/Solution.rs
+++ rust-leetcode/src/Solution.rs
@@ -5,11 +5,13 @@ use std::ops::Index;
 pub fn solutions() {
     interview_58_2();
     leetcode_1365();
+    leetcode_1342()
 }
 
 struct Solution;
 
 fn interview_58_2() {
+    println!("interview_58_2");
     impl Solution {
         pub fn reverse_left_words(s: String, n: i32) -> String {
             let mut s1 = String::from(&s[0..n as usize]);
@@ -24,6 +26,7 @@ fn interview_58_2() {
 }
 
 fn leetcode_1365() {
+    println!("leetcode_1365");
     impl Solution {
         pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
             let mut ret = Vec::with_capacity(nums.len());
@@ -43,6 +46,27 @@ fn leetcode_1365() {
     let nums = vec![8, 1, 2, 2, 3];
     let result = Solution::smaller_numbers_than_current(nums);
     for e in result.iter() {
-        print!("{}", e);
+        println!("{}", e);
     }
+}
+
+fn leetcode_1342() {
+    println!("leetcode_1342");
+    impl Solution {
+        pub fn number_of_steps(num: i32) -> i32 {
+            let mut n = num;
+            let mut i = 0;
+            while n != 0 {
+                if n & 1 == 0 {
+                    n /= 2;
+                } else {
+                    n -= 1;
+                }
+                i += 1;
+            };
+            i
+        }
+    }
+    let result = Solution::number_of_steps(8);
+    println!("{}", result);
 }
