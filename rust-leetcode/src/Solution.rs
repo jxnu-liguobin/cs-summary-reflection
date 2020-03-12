db699bcb681637730574cd5560930011f3508aab
梦境迷离 <568845948@qq.com>
2020-03-12 09:50:05 +0800

    add rust-leetcode

--- rust-leetcode/src/Solution.rs
+++ rust-leetcode/src/Solution.rs
@@ -5,11 +5,13 @@ use std::ops::Index;
 pub fn solutions() {
     interview_58_2();
     leetcode_1365();
-    leetcode_1342()
+    leetcode_1342();
+    leetcode_1313();
 }
 
 struct Solution;
 
+///左旋转字符串
 fn interview_58_2() {
     println!("interview_58_2");
     impl Solution {
@@ -25,6 +27,7 @@ fn interview_58_2() {
     println!("{}", result);
 }
 
+///有多少小于当前数字的数字
 fn leetcode_1365() {
     println!("leetcode_1365");
     impl Solution {
@@ -45,11 +48,10 @@ fn leetcode_1365() {
 
     let nums = vec![8, 1, 2, 2, 3];
     let result = Solution::smaller_numbers_than_current(nums);
-    for e in result.iter() {
-        println!("{}", e);
-    }
+    print_vec(result);
 }
 
+///将数字变成 0 的操作次数
 fn leetcode_1342() {
     println!("leetcode_1342");
     impl Solution {
@@ -69,4 +71,36 @@ fn leetcode_1342() {
     }
     let result = Solution::number_of_steps(8);
     println!("{}", result);
+}
+
+///解压缩编码列表
+fn leetcode_1313() {
+    println!("leetcode_1313");
+    impl Solution {
+        pub fn decompress_rl_elist(nums: Vec<i32>) -> Vec<i32> {
+            let mut rets = Vec::new();
+            for (index, e) in nums.iter().enumerate() {
+                if index & 1 == 0 {
+                    let mut freq = nums[index];
+                    let value = nums[index + 1];
+                    while freq != 0 {
+                        rets.push(value);
+                        freq -= 1;
+                    }
+                }
+            }
+            rets
+        }
+    }
+
+    let nums = vec![1, 2, 3, 4];
+
+    let result = Solution::decompress_rl_elist(nums);
+    print_vec(result);
+}
+
+fn print_vec(nums: Vec<i32>) {
+    for e in nums.iter() {
+        println!("{}", e);
+    }
 }
