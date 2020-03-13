e4a5353cbf1f7fbbbb9e411829171f552f046619
梦境迷离 <dreamylost@outlook.com>
2020-03-13 22:41:00 +0800

    add leetcode

---
+++ leetcode/src/main/java/io/github/dreamylost/Leetcode_LCP_1.java
@@ -0,0 +1,27 @@
+package io.github.dreamylost;
+
+/**
+ * LCP 1. 猜数字
+ * 此种题 不支持rust Scala
+ *
+ * @author 梦境迷离
+ * @version v1.0
+ * @since 2020-03-13
+ */
+public class Leetcode_LCP_1 {
+
+    public static void main(String[] args) {
+        int count = game(new int[]{1, 2, 3}, new int[]{1, 2, 3});
+        System.out.println(count);
+    }
+
+    public static int game(int[] guess, int[] answer) {
+        int flag = 0;
+        for (int j = 0; j < answer.length; j++) {
+            if (guess[j] == answer[j]) {
+                flag++;
+            }
+        }
+        return flag;
+    }
+}
---
+++ leetcode/src/main/scala/io/github/dreamylost/Leetcode_LCP_1_1.scala
@@ -0,0 +1,37 @@
+package io.github.dreamylost
+
+/**
+ *
+ * @author 梦境迷离
+ * @since 2020-03-13
+ * @version v1.0
+ */
+object Leetcode_LCP_1_1 extends App {
+
+  println(game(Array(1, 2, 3), Array(1, 2, 3)))
+
+  def game(guess: Array[Int], answer: Array[Int]): Int = {
+    var ret = 0
+    guess.zipWithIndex.map {
+      case (d, i) =>
+        answer.zipWithIndex.map {
+          case (d1, i1) => {
+            if (i == i1 && d == d1) {
+              ret += 1
+            }
+          }
+        }
+    }
+    ret
+  }
+
+  def game2(guess: Array[Int], answer: Array[Int]): Int = {
+    var r = 0
+    for (i <- guess.indices) {
+      if (guess(i) == answer(i)) {
+        r += 1
+      }
+    }
+    r
+  }
+}
