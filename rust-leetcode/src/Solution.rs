84f0af5e6f714ca28d797363cc5a507eced7a84e
梦境迷离 <dreamylost@outlook.com>
2020-03-16 22:10:49 +0800

    rust leetcode

--- rust-leetcode/src/Solution.rs
+++ rust-leetcode/src/Solution.rs
@@ -89,11 +89,11 @@ fn interview_02_02() {
         //倒数第k个，位置就是len-k。即快指针先走k步，然后2个指针同时走，快指针到达尾时，慢指针的位置就是第len-k个元素。此时快指针刚好走完一圈
         pub fn kth_to_last2(head: Option<Box<ListNode>>, k: i32) -> i32 {
             let mut i = k;
-            let mut fast = head.clone();
-            let mut slow = head;
+            let mut fast = head.as_ref();//clone也可以，但没有必要，不能copy，没有实现Copy
+            let mut slow = head.as_ref();
             while i > 0 {
                 if let Some(node) = fast.borrow() {
-                    fast = node.next.clone();
+                    fast = node.next.as_ref();
                     i -= 1;
                 } else {
                     break;
@@ -102,9 +102,9 @@ fn interview_02_02() {
 
             while fast != None {
                 if let Some(node) = fast.borrow() {
-                    fast = node.next.clone();
+                    fast = node.next.as_ref();
                     if let Some(node) = slow.borrow() {
-                        slow = node.next.clone();
+                        slow = node.next.as_ref();
                     }
                 }
             }
