f44eec579ce0cd8b820471ab10821ad4b8dba228
梦境迷离 <dreamylost@outlook.com>
2020-03-19 22:04:38 +0800

    fix code

---
+++ leetcode/src/main/java/io/github/dreamylost/Interview_02_03.java
@@ -0,0 +1,16 @@
+package io.github.dreamylost;
+
+/**
+ * 删除中间节点
+ *
+ * @author 梦境迷离
+ * @version v1.0
+ * @since 2020-03-19
+ */
+public class Interview_02_03 {
+    //将node后面的节点提前一个
+    public void deleteNode(ListNode node) {
+        node.value = node.next.value;
+        node.next = node.next.next;
+    }
+}
