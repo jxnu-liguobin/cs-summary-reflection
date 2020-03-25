ddf185bff991975905e93e3699bc8cd22e630f44
梦境迷离 <568845948@qq.com>
2020-03-25 18:19:29 +0800

    fix

---
+++ scala-examples/src/main/scala/io/github/dreamylost/examples/TestFold.scala
@@ -0,0 +1,22 @@
+package io.github.dreamylost.examples
+
+import scala.concurrent.{ Await, Future }
+import scala.concurrent.duration.Duration
+
+/**
+ *
+ * @author liguobin@growingio.com
+ * @version 1.0,2020/3/25
+ */
+object TestFold extends App {
+
+
+  val d = Option(111)
+  val empty = None
+
+  //fold[_]是不行的，无法推断类型。
+  val f = d.fold[Future[Option[_]]](Future.successful(None))(f => Future.successful(Option(f)))
+  val f2 = empty.fold[Future[Option[_]]](Future.successful(None))(f => Future.successful(Option(f)))
+  println(Await.result(f, Duration.Inf))
+  println(Await.result(f2, Duration.Inf))
+}
