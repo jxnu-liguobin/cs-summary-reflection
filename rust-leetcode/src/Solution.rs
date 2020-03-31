de16b8050acd4852272d816ebb66ca360a495f23
梦境迷离 <dreamylost@outlook.com>
2020-03-31 22:05:40 +0800

    rust leetcode

--- rust-leetcode/src/Solution.rs
+++ rust-leetcode/src/Solution.rs
@@ -860,6 +860,73 @@ fn leetcode_832() {
     }
 }
 
+///合并两个排序的链表
+fn interview_25() {
+    println!("interview_25");
+    impl Solution {
+        pub fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+            let mut result_head: Option<Box<ListNode>> = Some(Box::new(ListNode { val: -1, next: None }));
+            let mut cur = &mut result_head;
+            let mut l1 = l1;
+            let mut l2 = l2;
+            let mut next = true;
+            while l1.is_some() || l2.is_some() {
+                //take去除值，并保留为None
+                match (l1.take(), l2.take()) {
+                    (Some(_l1), None) => {
+                        //可变引用
+                        if let Some(ref mut _cur) = cur {
+                            _cur.next = Some(_l1);
+                        }
+                    },
+                    (None, Some(_l2)) => {
+                        if let Some(ref mut _cur) = cur {
+                            _cur.next = Some(_l2);
+                        }
+                    },
+                    (Some(mut _l1), Some(mut _l2)) => {
+                        if &_l1.val < &_l2.val {
+                            let _next = _l1.next.take();
+                            if let Some(ref mut _cur) = cur {
+                                //将l1拼接到cur后面
+                                _cur.next = Some(_l1);
+                                //移动cur本身
+                                cur = &mut _cur.next;
+                            }
+                            //移动链表l1，并将l2恢复
+                            l1 = _next;
+                            l2 = Some(_l2);
+                        } else {
+                            let _next = _l2.next.take();
+                            if let Some(ref mut _cur) = cur {
+                                _cur.next = Some(_l2);
+                                cur = &mut _cur.next;
+                            }
+                            l2 = _next;
+                            l1 = Some(_l1);
+                        }
+                    },
+                    (None, None) => {},
+                }
+            }
+            return result_head.unwrap().next;
+        }
+    }
+    let e1_1 = Some(Box::new(ListNode { val: 5, next: None }));
+    let e2_1 = Some(Box::new(ListNode { val: 4, next: e1_1 }));
+    let e3_1 = Some(Box::new(ListNode { val: 3, next: e2_1 }));
+    let e4_1 = Some(Box::new(ListNode { val: 2, next: e3_1 }));
+    let e5_1 = Some(Box::new(ListNode { val: 1, next: e4_1 }));
+
+    let e1 = Some(Box::new(ListNode { val: 5, next: None }));
+    let e2 = Some(Box::new(ListNode { val: 4, next: e1 }));
+    let e3 = Some(Box::new(ListNode { val: 3, next: e2 }));
+    let e4 = Some(Box::new(ListNode { val: 2, next: e3 }));
+    let e5 = Some(Box::new(ListNode { val: 1, next: e4 }));
+    let ret1 = Solution::merge_two_lists(e5, e5_1);
+    println!("{:?}", ret1);
+}
+
 ///所有方法调用
 pub fn solutions() {
     interview_58_2();
@@ -888,6 +955,7 @@ pub fn solutions() {
     leetcode_1304();
     lettcode_804();
     leetcode_832();
+    interview_25();
 }
 
 fn print_vec(nums: Vec<i32>) {
