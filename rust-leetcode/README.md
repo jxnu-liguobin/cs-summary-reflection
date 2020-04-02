ec2d238df6593d841dccbb88f47a98b1533a1937
梦境迷离 <dreamylost@outlook.com>
2020-04-02 21:27:47 +0800

    rust leetcode

--- rust-leetcode/README.md
+++ rust-leetcode/README.md
@@ -688,3 +688,53 @@ impl Solution {
     }
 }
 ```
+* 化栈为队
+```rust
+struct MyQueue {
+    stack1: VecDeque<Option<i32>>,
+    stack2: VecDeque<Option<i32>>,
+}
+impl MyQueue {
+    /** Initialize your data structure here. */
+    fn new() -> Self {
+        MyQueue {
+            stack1: VecDeque::new(),
+            stack2: VecDeque::new(),
+        }
+    }
+
+    /** Push element x to the back of queue. */
+    fn push(&mut self, x: i32) {
+        self.stack1.push_back(Some(x))
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    fn pop(&mut self) -> i32 {
+        return MyQueue::peek_pop(self, true)
+    }
+
+    fn peek_pop(queue: &mut MyQueue, flag: bool) -> i32 {
+        if queue.stack2.is_empty() {
+            while !queue.stack1.is_empty() {
+                queue.stack2.push_back(queue.stack1.pop_back().unwrap());
+            }
+        }
+        let ret = if flag {
+            queue.stack2.pop_back().unwrap()
+        } else {
+            queue.stack2.back().unwrap().clone()
+        };
+        ret.unwrap()
+    }
+
+    /** Get the front element. */
+    fn peek(&mut self) -> i32 {
+        return MyQueue::peek_pop(self, false)
+    }
+
+    /** Returns whether the queue is empty. */
+    fn empty(&mut self) -> bool {
+        self.stack1.is_empty() && self.stack2.is_empty()
+    }
+}
+```
--- rust-leetcode/src/Solution.rs
+++ rust-leetcode/src/Solution.rs
@@ -958,6 +958,67 @@ fn leetcode_1370() {
     println!("{}", ret);
 }
 
+///化栈为队
+fn interview_03_04() {
+    println!("interview_03_04");
+    struct MyQueue {
+        stack1: VecDeque<Option<i32>>,
+        stack2: VecDeque<Option<i32>>,
+    }
+    impl MyQueue {
+        /** Initialize your data structure here. */
+        fn new() -> Self {
+            MyQueue {
+                stack1: VecDeque::new(),
+                stack2: VecDeque::new(),
+            }
+        }
+
+        /** Push element x to the back of queue. */
+        fn push(&mut self, x: i32) {
+            self.stack1.push_back(Some(x))
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        fn pop(&mut self) -> i32 {
+            return MyQueue::peek_pop(self, true)
+        }
+
+        fn peek_pop(queue: &mut MyQueue, flag: bool) -> i32 {
+            if queue.stack2.is_empty() {
+                while !queue.stack1.is_empty() {
+                    queue.stack2.push_back(queue.stack1.pop_back().unwrap());
+                }
+            }
+            let ret = if flag {
+                queue.stack2.pop_back().unwrap()
+            } else {
+                queue.stack2.back().unwrap().clone()
+            };
+            ret.unwrap()
+        }
+
+        /** Get the front element. */
+        fn peek(&mut self) -> i32 {
+            return MyQueue::peek_pop(self, false)
+        }
+
+        /** Returns whether the queue is empty. */
+        fn empty(&mut self) -> bool {
+            self.stack1.is_empty() && self.stack2.is_empty()
+        }
+    }
+
+    let mut obj = MyQueue::new();
+    obj.push(1);
+    let ret_1: i32 = obj.peek();
+    let ret_2: i32 = obj.pop();
+    let ret_3: bool = obj.empty();
+    println!("{}", ret_1);
+    println!("{}", ret_2);
+    println!("{}", ret_3);
+}
+
 ///所有方法调用
 pub fn solutions() {
     interview_58_2();
@@ -988,6 +1049,7 @@ pub fn solutions() {
     leetcode_832();
     interview_25();
     leetcode_1370();
+    interview_03_04();
 }
 
 fn print_vec(nums: Vec<i32>) {
