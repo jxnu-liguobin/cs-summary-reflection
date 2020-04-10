2e8427ae906d44a3ec4e1235b644954d804f412d
梦境迷离 <568845948@qq.com>
2020-04-10 18:06:00 +0800

    rust leetcode - interview 09

--- rust-leetcode/README.md
+++ rust-leetcode/README.md
@@ -862,3 +862,37 @@ impl Solution {
     }
 }
 ```
+* 用两个栈实现队列
+```rust
+struct CQueue {
+    stack1: VecDeque<Option<i32>>,
+    stack2: VecDeque<Option<i32>>,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl CQueue {
+    fn new() -> Self {
+        CQueue {
+            stack1: VecDeque::new(),
+            stack2: VecDeque::new(),
+        }
+    }
+
+    fn append_tail(&mut self, value: i32) {
+        self.stack1.push_back(Some(value))
+    }
+
+    fn delete_head(&mut self) -> i32 {
+        if self.stack2.is_empty() {
+            while !self.stack1.is_empty() {
+                self.stack2.push_back(self.stack1.pop_back().unwrap());
+            }
+        }
+        let e = self.stack2.pop_back();
+        if e.is_none() { -1 } else { e.unwrap().unwrap() }
+    }
+}
+```
--- rust-leetcode/src/Solution.rs
+++ rust-leetcode/src/Solution.rs
@@ -1170,6 +1170,50 @@ fn interview_54() {
     println!("{:?}", Solution::kth_largest(root, 4))
 }
 
+///用两个栈实现队列
+fn interview_09() {
+    println!("interview_09");
+    struct CQueue {
+        stack1: VecDeque<Option<i32>>,
+        stack2: VecDeque<Option<i32>>,
+    }
+
+    /**
+     * `&self` means the method takes an immutable reference.
+     * If you need a mutable reference, change it to `&mut self` instead.
+     */
+    impl CQueue {
+        fn new() -> Self {
+            CQueue {
+                stack1: VecDeque::new(),
+                stack2: VecDeque::new(),
+            }
+        }
+
+        fn append_tail(&mut self, value: i32) {
+            self.stack1.push_back(Some(value))
+        }
+
+        fn delete_head(&mut self) -> i32 {
+            if self.stack2.is_empty() {
+                while !self.stack1.is_empty() {
+                    self.stack2.push_back(self.stack1.pop_back().unwrap());
+                }
+            }
+            let e = self.stack2.pop_back();
+            if e.is_none() { -1 } else { e.unwrap().unwrap() }
+        }
+    }
+
+    //Your CQueue object will be instantiated and called as such:
+    let mut obj = CQueue::new();
+    obj.append_tail(123);
+    let ret_2: i32 = obj.delete_head();
+    let ret_3: i32 = obj.delete_head();
+    println!("{}", ret_2);
+    println!("{}", ret_3)
+}
+
 ///所有方法调用
 pub fn solutions() {
     interview_58_2();
@@ -1206,6 +1250,7 @@ pub fn solutions() {
     interview_01_01();
     leetcode_1385();
     interview_54();
+    interview_09();
 }
 
 fn print_vec(nums: Vec<i32>) {
