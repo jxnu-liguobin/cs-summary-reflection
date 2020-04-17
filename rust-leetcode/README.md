1674a0ff05a42f901cb4704f3b6a5882de8061f3
梦境迷离 <568845948@qq.com>
2020-04-17 19:10:23 +0800

    rust leetcode 999

--- rust-leetcode/README.md
+++ rust-leetcode/README.md
@@ -7,6 +7,49 @@ Leetcode Rust 实现
 
 无注明，默认是LeetCode系列
 
+* 999 可以被一步捕获的棋子数
+```rust
+impl Solution {
+    pub fn num_rook_captures(board: Vec<Vec<char>>) -> i32 {
+        fn burnot(board: &Vec<Vec<char>>, x: i32, y: i32, direction: &Vec<i32>) -> bool {
+            let mut i = x;
+            let mut j = y;
+            let closures = move |i: i32, j: i32| -> bool { i >= 0 && i < 8 && j >= 0 && j < 8 };
+            while closures(i, j) {
+                //路被堵死
+                if board[i as usize][j as usize] == 'B' {
+                    break;
+                }
+                //是敌军
+                if board[i as usize][j as usize] == 'p' {
+                    return true;
+                }
+                i = i + direction[0];
+                j = j + direction[1];
+            }
+            return false
+        }
+
+        //定义方向数组，可以认为是四个方向向量，在棋盘问题上是常见的做法
+        let directions = vec![vec![-1, 0], vec![1, 0], vec![0, 1], vec![0, -1]];
+        for x in 0..8 {
+            for y in 0..8 {
+                if board[x][y] == 'R' {
+                    let mut res = 0;
+                    for direction in directions.iter() {
+                        if burnot(&board, x as i32, y as i32, direction) {
+                            res += 1;
+                        }
+                    }
+                    return res;
+                }
+            }
+        }
+
+        return 0;
+    }
+}
+```
 * 1403 非递增顺序的最小子序列
 ```rust
 impl Solution {
--- rust-leetcode/src/Solution.rs
+++ rust-leetcode/src/Solution.rs
@@ -1483,6 +1483,56 @@ fn leetcode_557() {
     println!("{}", ret);
 }
 
+///可以被一步捕获的棋子数
+fn leetcode_999() {
+    println!("leetcode_999");
+    impl Solution {
+        pub fn num_rook_captures(board: Vec<Vec<char>>) -> i32 {
+            fn burnot(board: &Vec<Vec<char>>, x: i32, y: i32, direction: &Vec<i32>) -> bool {
+                let mut i = x;
+                let mut j = y;
+                let closures = move |i: i32, j: i32| -> bool { i >= 0 && i < 8 && j >= 0 && j < 8 };
+                while closures(i, j) {
+                    //路被堵死
+                    if board[i as usize][j as usize] == 'B' {
+                        break;
+                    }
+                    //是敌军
+                    if board[i as usize][j as usize] == 'p' {
+                        return true;
+                    }
+                    i = i + direction[0];
+                    j = j + direction[1];
+                }
+                return false
+            }
+
+            //定义方向数组，可以认为是四个方向向量，在棋盘问题上是常见的做法
+            let directions = vec![vec![-1, 0], vec![1, 0], vec![0, 1], vec![0, -1]];
+            for x in 0..8 {
+                for y in 0..8 {
+                    if board[x][y] == 'R' {
+                        let mut res = 0;
+                        for direction in directions.iter() {
+                            if burnot(&board, x as i32, y as i32, direction) {
+                                res += 1;
+                            }
+                        }
+                        return res;
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+
+    let d = vec![vec!['.', '.', '.', '.', '.', '.', '.', '.'], vec!['.', '.', '.', 'p', '.', '.', '.', '.'], vec!['.', '.', '.', 'R', '.', '.', '.', 'p'], vec!['.', '.', '.', '.', '.', '.', '.', '.'], vec!['.', '.', '.', '.', '.', '.', '.', '.'], vec!['.', '.', '.', 'p', '.', '.', '.', '.'],
+                 vec!['.', '.', '.', '.', '.', '.', '.', '.'], vec!['.', '.', '.', '.', '.', '.', '.', '.']];
+    let ret = Solution::num_rook_captures(d);
+    println!("{}", ret)
+}
+
 ///所有方法调用
 pub fn solutions() {
     interview_58_2();
@@ -1530,6 +1580,7 @@ pub fn solutions() {
     leetcode_1374();
     leetcode_1403();
     leetcode_557();
+    leetcode_999();
 }
 
 fn print_vec(nums: Vec<i32>) {
