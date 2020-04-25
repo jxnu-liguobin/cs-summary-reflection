21f896c6613f96b780e299efa766592ce40d8ccc
梦境迷离 <dreamylost@outlook.com>
2020-04-25 20:56:25 +0800

    rust leetcode 9

--- rust-leetcode/README.md
+++ rust-leetcode/README.md
@@ -1241,3 +1241,38 @@ impl Solution {
     }
 }
 ```
+* 9 回文数
+```rust
+impl Solution {
+    //1.双指针，2.找规律，3.字符串，4.栈
+    pub fn is_palindrome(x: i32) -> bool {
+        //4ms
+        let mut n = x;
+        if n < 0 || (n != 0 && n % 10 == 0) { return false; } else {
+            let mut tmp = 0;
+            while n > 0 {
+                tmp = tmp * 10 + (n % 10);
+                n /= 10;
+            }
+            return tmp == x;
+        }
+    }
+
+    pub fn is_palindrome2(x: i32) -> bool {
+        //直接to_string后index需要8ms
+        //let mut nums: Vec<char> = x.to_string().chars().map(|x| x.to_owned() ).collect();
+        //nums[i] != nums[j] 需要16ms
+        let mut nums = x.to_string();
+        let mut i = 0;
+        let mut j = nums.len() - 1;
+        while i < j {
+            if nums.index(i..=i) != nums.index(j..=j) {
+                return false;
+            }
+            i += 1;
+            j -= 1;
+        }
+        true
+    }
+}
+```
--- rust-leetcode/src/Solution.rs
+++ rust-leetcode/src/Solution.rs
@@ -1684,6 +1684,48 @@ fn leetcode_944() {
     println!("{}", ret);
 }
 
+
+///回文数
+fn leetcoode_9() {
+    println!("leetcoode_9");
+    impl Solution {
+        //1.双指针，2.找规律，3.字符串，4.栈
+        pub fn is_palindrome(x: i32) -> bool {
+            //4ms
+            let mut n = x;
+            if n < 0 || (n != 0 && n % 10 == 0) { return false; } else {
+                let mut tmp = 0;
+                while n > 0 {
+                    tmp = tmp * 10 + (n % 10);
+                    n /= 10;
+                }
+                return tmp == x;
+            }
+        }
+
+        pub fn is_palindrome2(x: i32) -> bool {
+            //直接to_string后index需要8ms
+            //let mut nums: Vec<char> = x.to_string().chars().map(|x| x.to_owned() ).collect();
+            //nums[i] != nums[j] 需要16ms
+            let mut nums = x.to_string();
+            let mut i = 0;
+            let mut j = nums.len() - 1;
+            while i < j {
+                if nums.index(i..=i) != nums.index(j..=j) {
+                    return false;
+                }
+                i += 1;
+                j -= 1;
+            }
+            true
+        }
+    }
+    let ret = Solution::is_palindrome(181);
+    let ret2 = Solution::is_palindrome2(181);
+    println!("{}", ret);
+    println!("{}", ret2);
+}
+
 ///所有方法调用
 pub fn solutions() {
     interview_58_2();
@@ -1737,6 +1779,7 @@ pub fn solutions() {
     leetcode_1413();
     interview_32_2();
     leetcode_944();
+    leetcoode_9();
 }
 
 fn print_vec(nums: Vec<i32>) {
