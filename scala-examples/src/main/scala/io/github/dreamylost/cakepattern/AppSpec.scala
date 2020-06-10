25320f8946a33b841bcd56610cbcaf2a11d63d3f
梦境迷离 <dreamylost@outlook.com>
2020-06-10 13:24:11 +0800

    scala cake pattern examples

---
+++ scala-examples/src/main/scala/io/github/dreamylost/cakepattern/AppSpec.scala
@@ -0,0 +1,18 @@
+package io.github.dreamylost.cakepattern
+
+/**
+  *
+ * @author 梦境迷离
+  * @version 1.0,2020/6/10
+  */
+object AppSpec extends App {
+
+  APP.car.drive()
+  APP.car.park()
+}
+
+object APP extends CarComponent with EngineComponent {
+  override val car: APP.Car = new HondaCar() //实例化时使用了engine
+  override val engine: APP.Engine = new DieselEngine()
+
+}
---
+++ scala-examples/src/main/scala/io/github/dreamylost/cakepattern/EngineComponent.scala
@@ -0,0 +1,65 @@
+package io.github.dreamylost.cakepattern
+
+import io.github.dreamylost.cakepattern.FuelType.FuelType
+
+/**
+  *
+ * @author 梦境迷离
+  * @version 1.0,2020/6/10
+  */
+trait EngineComponent {
+
+  trait Engine {
+
+    private var running = false
+
+    def start(): Unit = {
+      println("Engine start")
+    }
+
+    def stop(): Unit = {
+      println("Engine stop")
+    }
+
+    def isRunning: Boolean = running
+
+    def fuelType: FuelType
+  }
+
+  val engine: Engine
+
+  protected class DieselEngine extends Engine {
+    override val fuelType = FuelType.Diesel
+  }
+
+}
+
+object FuelType extends Enumeration {
+  type FuelType = Value
+  val Diesel = Value
+}
+
+//需要混入了EngineComponent的才能使用CarComponent
+trait CarComponent {
+  this: EngineComponent =>
+
+  trait Car {
+    def drive(): Unit
+
+    def park(): Unit
+  }
+
+  val car: Car
+
+  protected class HondaCar extends Car {
+    override def drive() {
+      engine.start()
+      println("Vroom vroom")
+    }
+
+    override def park() {
+      println("HondaCar")
+    }
+  }
+
+}
