9033d143a8466da2e8bee1e52ddfd7a00d0e1ab5
laozhang <1044985343@qq.com>
2020-06-13 20:16:56 +0800

    add python 94 and python 1008

---
+++ python-leetcode/solution/tree/leetcode_94_.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# coding=utf-8
+"""
+94. 二叉树的中序遍历
+"""
+from solution import TreeNode
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        stack = []
+        list = []
+        while root or len(stack) > 0:
+            while root:
+                stack.append(root)
+                root = root.left
+
+            node = stack.pop()
+            list.append(node.val)
+            root = node.right
+
+        return list
