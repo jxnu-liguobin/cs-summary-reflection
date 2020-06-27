91937f31e58c824559f9e7f18a67d5f245b04e1e
laozhang <1044985343@qq.com>
2020-06-27 20:37:02 +0800

    add python 116

--- python-leetcode/solution/__init__.py
+++ python-leetcode/solution/__init__.py
@@ -14,3 +14,11 @@ class Node:
     def __init__(self, val=None, children=None):
         self.val = val
         self.children = children
+
+
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
