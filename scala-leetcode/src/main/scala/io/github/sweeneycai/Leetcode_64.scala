c4eceec1b518e6fc7173832ab03657bfa2570671
梦境迷离 <dreamylost@outlook.com>
2020-08-15 12:01:02 +0800

    fix code style

--- scala-leetcode/src/main/scala/io/github/sweeneycai/Leetcode_64.scala
+++ scala-leetcode/src/main/scala/io/github/sweeneycai/Leetcode_64.scala
@@ -1,17 +1,19 @@
+/* Licensed under Apache-2.0 (C) All Contributors */
 package io.github.sweeneycai
 
 /**
- * 64. 最小路径和(Medium)
- *
- * 给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
- *
- * 说明：每次只能向下或者向右移动一步。
- */
+  * 64. 最小路径和(Medium)
+  *
+  * 给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+  *
+  * 说明：每次只能向下或者向右移动一步。
+  */
 object Leetcode_64 {
+
   /**
-   * 递推公式：
-   * f(i)(j) = grid(i)(j) + min(grid(i - 1)(j), grid(i)(j - 1))
-   */
+    * 递推公式：
+    * f(i)(j) = grid(i)(j) + min(grid(i - 1)(j), grid(i)(j - 1))
+    */
   def minPathSum(grid: Array[Array[Int]]): Int = {
     if (grid.isEmpty) return 0
     val copy = grid.clone() // 不修改原数组
@@ -28,12 +30,14 @@ object Leetcode_64 {
   }
 
   def main(args: Array[String]): Unit = {
-    println(minPathSum(
-      Array(
-        Array(1, 3, 1),
-        Array(1, 5, 6),
-        Array(4, 2, 1)
+    println(
+      minPathSum(
+        Array(
+          Array(1, 3, 1),
+          Array(1, 5, 6),
+          Array(4, 2, 1)
+        )
       )
-    ))
+    )
   }
 }
