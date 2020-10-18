90d91675c5854b565d412cf337f7b1bded56ddfe
梦境迷离 <dreamylost@outlook.com>
2020-10-18 18:30:01 +0800

    add

---
+++ scala-utils/src/main/scala/io/github/dreamylost/other/AtomicLogic.scala
@@ -0,0 +1,33 @@
+package io.github.dreamylost.other
+
+import java.util.concurrent.atomic.AtomicReference
+
+/**
+ *
+ * @see [[https://github.com/jxnu-liguobin]]
+ * @author 梦境迷离
+ * @since 2020-10-18
+ * @version 1.0
+ */
+@unchecked
+object AtomicLogic {
+
+  def compareAndSetSync[T](ref: AtomicReference[T])(logic: T => T) {
+    while (true) {
+      val snapshot = ref.get
+      val update = logic(snapshot) //无副作用的函数
+      if (ref.compareAndSet(snapshot, update)) return
+    }
+  }
+
+  def compareSync[T, V](ref: AtomicReference[T])(logic: T => V): V = {
+    var snapshot = ref.get
+    var result = logic(snapshot)
+    while (snapshot != ref.get) {
+      snapshot = ref.get
+      result = logic(snapshot)
+    }
+    result
+  }
+
+}
