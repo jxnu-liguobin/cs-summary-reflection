e6cd9ceb21b879c986f45b2a193ae4445b91798b
梦境迷离 <dreamylost@outlook.com>
2020-11-17 09:59:58 +0800

    fix comment

--- kotlin-leetcode/src/main/kotlin/io/github/dreamylost/Leetcode_406.kt
+++ kotlin-leetcode/src/main/kotlin/io/github/dreamylost/Leetcode_406.kt
@@ -15,8 +15,7 @@ class Leetcode_406 {
 
     companion object {
         /**
-         * 先对输入数组排序，h升序，k降序 从头循环遍历 当前这个人就是剩下未安排的人中最矮的人，他的k值就代表他在剩余空位的索引值
-         * 如果有多个人高度相同，要按照k值从大到小领取索引值
+         * 先对输入数组排序，按照h降序，k升序。
          * 324 ms,66.67%
          * 44.5 MB,16.67%
          */
@@ -24,14 +23,14 @@ class Leetcode_406 {
             if (people.isEmpty() || people[0].isEmpty())
                 return emptyArray()
             people.sortWith(
-                    Comparator { o1, o2 ->
-                        if (o1[0] == o2[0]) o1[1] - o2[1] else o2[0] - o1[0]
-                    }
+                Comparator { o1, o2 ->
+                    if (o1[0] == o2[0]) o1[1] - o2[1] else o2[0] - o1[0]
+                }
             )
 
             val ans: MutableList<IntArray> = ArrayList()
             for (person in people) {
-                // 记录k和其原值
+                // 身高低的插入到身高高的前面，不会影响身高高的k值
                 ans.add(person[1], person)
             }
             return ans.toTypedArray()
@@ -40,12 +39,12 @@ class Leetcode_406 {
         @JvmStatic
         fun main(args: Array<String>) {
             val ret = reconstructQueue(
-                    arrayOf(
-                            arrayOf(5, 0).toIntArray(),
-                            arrayOf(7, 0).toIntArray(), arrayOf(5, 2).toIntArray(),
-                            arrayOf(6, 1).toIntArray(), arrayOf(4, 4).toIntArray(),
-                            arrayOf(7, 1).toIntArray()
-                    )
+                arrayOf(
+                    arrayOf(5, 0).toIntArray(),
+                    arrayOf(7, 0).toIntArray(), arrayOf(5, 2).toIntArray(),
+                    arrayOf(6, 1).toIntArray(), arrayOf(4, 4).toIntArray(),
+                    arrayOf(7, 1).toIntArray()
+                )
             )
             ret.forEach {
                 it.forEach {
