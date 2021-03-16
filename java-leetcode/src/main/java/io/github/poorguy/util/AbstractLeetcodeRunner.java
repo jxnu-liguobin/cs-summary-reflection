e304ee9b9c772516e7a28e20b689599b8af185c8
梦境迷离 <dreamylost@outlook.com>
2021-03-16 14:50:17 +0800

    fix build config and java  util

--- java-leetcode/src/main/java/io/github/poorguy/util/AbstractLeetcodeRunner.java
+++ java-leetcode/src/main/java/io/github/poorguy/util/AbstractLeetcodeRunner.java
@@ -2,7 +2,7 @@
 package io.github.poorguy.util;
 
 /**
- * @author liguobin@growingio.com
+ * @author 梦境迷离
  * @version 1.0, 2021/3/16
  */
 public interface AbstractLeetcodeRunner<Invoker, F, P, E> {}
